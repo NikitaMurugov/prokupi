6415c411697453bf4bd98b17693f08ab832a8dc4
Exsist <roma.kosarev21@yandex.ru>
2020-05-25 00:14:21 +0500

    job in validation part 1

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,6 +2,7 @@
 
 @section('title')Регистрация@endsection
 
+
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
@@ -51,19 +52,7 @@
                                 </div>
                             </div>
 
-                            <div class="form-group row">
-                                <label for="avatar" class="col-md-4 col-form-label text-md-right">{{ __('Ваша фотография') }}</label>
 
-                                <div class="col-md-6">
-                                    <input id="avatar" type="file" class="form-control-file @error('avatar') is-invalid @enderror" name="avatar">
-
-                                    @error('avatar')
-                                    <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                    @enderror
-                                </div>
-                            </div>
 
                             <div class="form-group row">
                                 <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
